510132aa2f750cceb46a04833af5a15e5d093dac
aita <ryo.aita@gunosy.com>
2017-11-14 23:46:17 +0900

    Rewrite echo server with socket module

--- sample/echo.py
+++ sample/echo.py
@@ -1,32 +1,25 @@
-import asyncio
+import socket
 
-async def handle_echo(reader, writer):
-    data = await reader.read(1024)
-    message = data.decode()
-    addr = writer.get_extra_info('peername')
-    print("received %r from %r" % (message, addr))
 
-    print("send: %r" % message)
-    writer.write(data)
-    await writer.drain()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('127.0.0.1', 8888))
+sock.listen(1)
 
-    print("close the client socket")
-    writer.close()
 
-loop = asyncio.get_event_loop()
-coro = asyncio.start_server(handle_echo, '127.0.0.1', 8888, loop=loop)
-server = loop.run_until_complete(coro)
-
-# Serve requests until Ctrl+C is pressed
-print('serving on {}'.format(server.sockets[0].getsockname()))
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+print('serving on {}'.format(sock.getsockname()))
+running = True
+while running:
+    try:
+        client, addr = sock.accept()
+        message = client.recv(1024)
+        print("received %r from %r" % (message, addr))
+        print("send: %r" % message)
+        client.send(message)
+        client.close()
+    except KeyboardInterrupt:
+        break
 
 print("closing echo server")
 # Close the server
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+sock.close()
 
