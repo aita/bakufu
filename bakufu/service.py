2147994e88815d0b85c84b0c1327be5716c547b1
aita <ryo.aita@gunosy.com>
2017-11-16 23:22:03 +0900

    Add auto restarting

--- bakufu/service.py
+++ bakufu/service.py
@@ -1,23 +1,13 @@
-import errno
-import os
-import signal
-import sys
-from enum import Enum
-from subprocess import Popen, PIPE
 from tornado import gen
 
 from bakufu import logger
-
-
-class ProcessState(Enum):
-    stopped = "stopped"
-    starting = "starting"
-    running = "running"
-    stopping = "stopping"
+from bakufu.process import Process
 
 
 class Service:
-    def __init__(self, name, command, num_processes=1, stop_signal="SIGTERM", **options):
+    def __init__(self, name, command,
+                 num_processes=1, stop_signal="SIGTERM",
+                 **options):
         self.name = name
         self.command = command
         self.num_processes = num_processes
@@ -25,70 +15,41 @@ class Service:
         self._options = options
 
         self.processes = {}
-        self.status = ProcessState.stopped
         self.use_sockets = False
 
-    @gen.coroutine
     def start(self):
-        self.status = ProcessState.starting
         for _ in range(self.num_processes):
             self.spawn_process()
-        self.status = ProcessState.running
         logger.info("%s started" % self.name)
 
     def spawn_process(self):
-        process = Popen(
+        process = Process(
             self.command,
-            shell=True,
-            # stdout=PIPE,
-            # stderr=PIPE,
-            stdout=sys.stdout,
-            stderr=sys.stderr,
-            close_fds=not self.use_sockets,
+            stop_signal=self.stop_signal,
         )
+        process.spawn()
         self.processes[process.pid] = process
+        return process
 
     @gen.coroutine
     def stop(self):
-        if self.status == ProcessState.stopped:
-            return
-
-        logger.info("stopping the %s service" % self.name)
-        self.status.status = ProcessState.stopping
+        logger.info("stopping %s service" % self.name)
         for process in self.processes.values():
-            process.send_signal(getattr(signal, self.stop_signal))
+            process.kill()
 
-        yield self.reap_processes()
-        logger.info("all %s processes stopped" % self.name)
-
-    @gen.coroutine
-    def reap_processes(self):
+        reaping = []
         for pid in list(self.processes):
-            yield self.reap_process(pid)
+            process = self.processes.pop(pid)
+            reaping.append(process.reap())
+        yield reaping
 
-    @gen.coroutine
-    def reap_process(self, pid):
-        process = self.processes.pop(pid)
-        while True:
-            try:
-                pid_, _ = os.waitpid(pid, os.WNOHANG)
-                if pid_ != pid:
-                    # the child process is still alive
-                    yield gen.sleep(0.001)
-                    continue
-                else:
-                    break
-            except OSError as e:
-                if e.errno == errno.EINTR:
-                    # waitpid was interuppted
-                    continue
-                elif e.errno == errno.ECHILD:
-                    # the child has gone
-                    break
-                else:
-                    raise e
+    def watch_processes(self):
+        dead_processes = []
+        for process in self.processes.values():
+            if not process.watch():
+                logger.error("%s process is dead: pid=%s" % (self.name, process.pid))
+                dead_processes.append(process)
 
-        if process.poll() is None:
-            logger.warn("process[pid=%d] is still alive" % pid)
-            process.terminate()
-        logger.info("process[pid=%d] exited with code %d" % (pid, process.returncode))
+        for process in dead_processes:
+            process.spawn()
+            logger.info("respawn %s process: pid=%s" % (self.name, process.pid))
